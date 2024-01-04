7ff69dcf71a5de79691565d25b9bd7350d693c6e
Vaidotak <pehtrauhk@gmail.com>
2024-01-04 15:14:19 +0200

    base-devel

--- start.sh
+++ start.sh
@@ -15,7 +15,7 @@ cd yay
 makepkg -si --noconfirm
 cd ..
 
-# jau bus sistemoje sudo pacman -S --needed base-devel
+sudo pacman -S --needed base-devel
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si --noconfirm
